3aedb14c92f5c60dc0f26c62668c8df52e67b492
Rajan <shakyrajan009@gmail.com>
2026-05-11 02:11:03 +0530

    schedulling

--- run.bat
+++ run.bat
@@ -1,8 +1,10 @@
 @echo off
 
-echo ============================== >> logs.txt
-echo Run Time: %date% %time% >> logs.txt
+cd /d "D:\Games\New folder (2)\Projects\New folder (2)"
 
 call .venv\Scripts\activate
+
+echo ============================== >> logs.txt
+echo Run Time: %date% %time% >> logs.txt
 
 python app.py >> logs.txt 2>&1
