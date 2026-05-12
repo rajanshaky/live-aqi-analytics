b2a42eea81cf20341008e2698e5bc04a7f34675a
Rajan <shakyrajan009@gmail.com>
2026-05-12 17:59:26 +0530

    configure_logs

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ cd /d "D:\Games\New folder (2)\Projects\New folder (2)"
 
 call .venv\Scripts\activate
 
-echo ============================== >> logs.txt
-echo Run Time: %date% %time% >> logs.txt
+echo ============================== >> logs\pipeline_logs.txt
+echo Run Time: %date% %time% >> logs\pipeline_logs.txt
 
-python app.py >> logs.txt 2>&1
+python app.py >> logs\pipeline_logs.txt 2>&1
