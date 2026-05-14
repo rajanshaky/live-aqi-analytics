39b35b46cbdac01f7904d5dc1455d7acb2a1cc84
Rajan <shakyrajan009@gmail.com>
2026-05-15 01:55:33 +0530

    dashboard

--- run.bat
+++ run.bat
@@ -1,10 +1,13 @@
 @echo off
 
-cd /d "D:\Games\New folder (2)\Projects\New folder (2)"
+cd /d "D:\Games\New folder (2)\Projects\live_aqi_analytics"
 
 call .venv\Scripts\activate
 
 echo ============================== >> logs\pipeline_logs.txt
 echo Run Time: %date% %time% >> logs\pipeline_logs.txt
 
-python app.py >> logs\pipeline_logs.txt 2>&1
+python main.py >> logs\pipeline_logs.txt 2>&1
+
+echo. >> logs\pipeline_logs.txt
+
