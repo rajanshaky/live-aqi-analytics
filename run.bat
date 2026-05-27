730fd609c2454cc5212b2c178f58d9561e5ad397
Rajan <shakyrajan009@gmail.com>
2026-05-27 23:57:28 +0530

    updated batch file

--- run.bat
+++ run.bat
@@ -1,13 +1,12 @@
 @echo off
 
-cd /d "D:\Games\New folder (2)\Projects\live_aqi_analytics"
+cd /d "D:\Games\New folder (2)\Projects\live-aqi-analytics"
 
-call .venv\Scripts\activate
+if not exist logs mkdir logs
 
 echo ============================== >> logs\pipeline_logs.txt
 echo Run Time: %date% %time% >> logs\pipeline_logs.txt
 
-python main.py >> logs\pipeline_logs.txt 2>&1
+.venv\Scripts\python.exe main.py >> logs\pipeline_logs.txt 2>&1
 
 echo. >> logs\pipeline_logs.txt
-
